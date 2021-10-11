a3ebb4731dbe3fc475ae0b271aa8d3211fc41616
masterUNG <phrombutr@gmail.com>
2021-10-11 12:28:06 +0700

    update 11Oct

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:findfault/XD_intro.dart';
+import 'package:findfault/states/authen.dart';
+import 'package:findfault/states/ceate_database.dart';
+import 'package:findfault/states/require_data.dart';
+import 'package:findfault/states/show_map.dart';
+import 'package:flutter/material.dart';
+import 'package:adobe_xd/pinned.dart';
+
+final Map<String, WidgetBuilder> map = {
+  // '/authen': (BuildContext context) => Authen(),
+  '/requireData': (BuildContext context) => ReqireData(),
+  '/createdatabase': (BuildContext context) => CreateDatabase(),
+  '/intro': (BuildContext context) => XD_intro()
+};
+
+String? firstState;
+
+void main() {
+  firstState = '/intro';
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      routes: map,
+      initialRoute: firstState,
+    );
+  }
+}
